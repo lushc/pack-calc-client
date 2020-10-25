07ada5f56af927325df32acda99910df495a4d8e
Chris Lush <chris.lush@gmail.com>
2020-10-25 20:11:39 +0000

    Add Github link in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
-import { Heading, Flex } from "@chakra-ui/core";
+import { Heading, Flex, Link, Box } from "@chakra-ui/core";
+import { GoMarkGithub } from "react-icons/go";
 
 const Header = () => (
   <Flex
@@ -13,6 +14,10 @@ const Header = () => (
     <Heading as="h1" size="lg">
       Pack Calculator
     </Heading>
+    <Link href="https://github.com/lushc/pack-calc-go" isExternal>
+      <Box display="inline-block" mr={2} as={GoMarkGithub} />
+      lushc/pack-calc-go
+    </Link>
   </Flex>
 );
 
